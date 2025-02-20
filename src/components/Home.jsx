0af16a200a9ccf1ef45b36da03451346f45edd25
AdityaZulkarnaen <adityazulkarnaen083@gmail.com>
2025-02-20 15:15:13 +0700

    skill dah jadi ya

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <div className='h-5/12 w-6/12 overflow-x-visible'>
                 <div className='text-6xl text-white mb-12'>Hi There! My name is Adit</div>
                 <div className='text-2xl text-white mb-3'>I'm a passionate Software Engineer with a strong interest in web development, software development, design, data science, and machine learning. I enjoy building innovative solutions, optimizing user experiences, and leveraging data-driven insights to create impactful applications.</div>
-                <a className='flex text-purple-700 text-2xl' href="">See my resume </a>
+                <a className='flex text-purple-700 text-2xl hover:text-3xl transition-all duration-700' href="../../public/cvadit.pdf" target='blank'>See my resume →</a>
             </div>
         </div>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import github from '../assets/github-logo.png';
+import react from '../assets/react.svg'
+import tailwind from '../assets/tailwind.svg'
+import express from '../assets/express.svg'
+import js from '../assets/JavaScript.svg'
+import sql from '../assets/sql.svg'
+import html from '../assets/html-1.svg'
+import css from '../assets/css.svg'
+import figma from '../assets/figma.svg'
+import python from '../assets/python.svg'
 
 const Skills = () => {
   return (
@@ -7,17 +17,35 @@ const Skills = () => {
             <div className='text-9xl text-white justify-self-center mt-50 mb-20'>
                 MY SKILLS
             </div>
-            <div className='w-4/5 h-4/5 justify-self-center mt-15'>
-                <div className="w-[100%] h-[100%] flex gap-[8%] flex-wrap content-stretch items-stretch justify-center justify-items-center flex-row">
-                    <div className="w-[23.5%] h-[23.5%] border-purple-900 border-3 rounded-xl"></div>
-                    <div className="w-[23.5%] h-[23.5%] border-purple-900 border-3 rounded-xl"></div>
-                    <div className="w-[23.5%] h-[23.5%] border-purple-900 border-3 rounded-xl"></div>
-                    <div className="w-[23.5%] h-[23.5%] border-purple-900 border-3 rounded-xl"></div>
-                    <div className="w-[23.5%] h-[23.5%] border-purple-900 border-3 rounded-xl"></div>
-                    <div className="w-[23.5%] h-[23.5%] border-purple-900 border-3 rounded-xl"></div>
-                    <div className="w-[23.5%] h-[23.5%] border-purple-900 border-3 rounded-xl"></div>
-                    <div className="w-[23.5%] h-[23.5%] border-purple-900 border-3 rounded-xl"></div>
-                    <div className="w-[23.5%] h-[23.5%] border-purple-900 border-3 rounded-xl"></div>
+            <div className='w-[98%] h-[98%] justify-self-center mt-15'>
+                <div className="w-[90%] h-[90%] flex gap-[8%] flex-wrap content-stretch items-stretch justify-center justify-self-center justify-items-center flex-row">
+                    <div className="w-[23.5%] h-[23.5%] border-purple-950 shadow-purple-800 shadow-lg border-3 rounded-xl text-white">
+                        <div className='flex items-center h-[100%] text-2xl'> <img className='w-15 h-15 ml-4 mr-4' src={html} alt="" /> HTML</div>
+                    </div>
+                    <div className="w-[23.5%] h-[23.5%] border-purple-950 shadow-purple-800 shadow-lg border-3 rounded-xl text-white">
+                        <div className='flex items-center h-[100%] text-2xl'> <img className='w-15 h-15 ml-4 mr-4' src={css} alt="" /> CSS</div>
+                    </div>
+                    <div className="w-[23.5%] h-[23.5%] border-purple-950 shadow-purple-800 shadow-lg border-3 rounded-xl text-white">
+                        <div className='flex items-center h-[100%] text-2xl'> <img className='w-15 h-15 ml-4 mr-4' src={js} alt="" /> JavaScript</div>
+                    </div>
+                    <div className="w-[23.5%] h-[23.5%] border-purple-950 shadow-purple-800 shadow-lg border-3 rounded-xl text-white">
+                        <div className='flex items-center h-[100%] text-2xl'> <img className='w-15 h-15 ml-4 mr-4' src={python} alt="" /> Python</div>
+                    </div>
+                    <div className="w-[23.5%] h-[23.5%] border-purple-950 shadow-purple-800 shadow-lg border-3 rounded-xl text-white">
+                        <div className='flex items-center h-[100%] text-2xl'> <img className='w-15 h-15 ml-4 mr-4' src={react} alt="" /> React</div>
+                    </div>
+                    <div className="w-[23.5%] h-[23.5%] border-purple-950 shadow-purple-800 shadow-lg border-3 rounded-xl text-white">
+                        <div className='flex items-center h-[100%] text-2xl'> <img className='w-15 h-15 ml-4 mr-4' src={tailwind} alt="" /> Tailwind</div>
+                    </div>
+                    <div className="w-[23.5%] h-[23.5%] border-purple-950 shadow-purple-800 shadow-lg border-3 rounded-xl text-white">
+                        <div className='flex items-center h-[100%] text-2xl'> <img className='w-15 h-15 ml-4 mr-4' src={express} alt="" /> Express</div>
+                    </div>
+                    <div className="w-[23.5%] h-[23.5%] border-purple-950 shadow-purple-800 shadow-lg border-3 rounded-xl text-white">
+                        <div className='flex items-center h-[100%] text-2xl'> <img className='w-15 h-15 ml-4 mr-4' src={sql} alt="" /> SQL</div>
+                    </div>
+                    <div className="w-[23.5%] h-[23.5%] border-purple-950 shadow-purple-800 shadow-lg border-3 rounded-xl text-white">
+                        <div className='flex items-center h-[100%] text-2xl'> <img className='w-15 h-15 ml-4 mr-4' src={figma} alt="" /> Figma</div>
+                    </div>
                 </div>
             </div>
 
